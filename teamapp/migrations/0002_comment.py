ed0642e24c2e24a31b3492f2dc4cb2e8df942172
s1f10210293 <85635699+s1f10210293@users.noreply.github.com>
2021-12-17 13:47:00 +0900

    Update 0002_comment.py

--- teamapp/migrations/0002_comment.py
+++ teamapp/migrations/0002_comment.py
@@ -8,7 +8,7 @@ import django.utils.timezone
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0001_initial'),
+        ('teamapp', '0001_initial'),
     ]
 
     operations = [
