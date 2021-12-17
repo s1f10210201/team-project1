18ce5dc1a28dc4378a819377525cd0753067e71e
s1f10210293 <85635699+s1f10210293@users.noreply.github.com>
2021-12-17 13:22:13 +0900

    じゃっじゃじゃ

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as blog_views
+from teamapp import views as teamapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
