59daf3106654293a2da5c49ccf6805cff73c3f21
s1f102100142 <85726716+s1f102100142@users.noreply.github.com>
2021-12-20 16:24:03 +0900

    mm

--- teamapp/urls.py
+++ teamapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:article_id>/update', views.update, name='update'),
 ]
--- teamapp/views.py
+++ teamapp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'teamapp/index.html')
+
+def update(request, article_id):
+	return HttpResponse("article_id: {}".format(article_id))
