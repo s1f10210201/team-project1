eb09a76b9185b403e57ffead15b68696e1ea085b
s1f10210293 <85635699+s1f10210293@users.noreply.github.com>
2021-12-31 19:26:08 +0900

    コピペ

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TeamappConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teamapp'
+    name = 'blog'
--- teamapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return render(request, 'teamapp/index.html')
-
-def update(request, article_id):
-    return HttpResponse("article_id: {}".format(article_id))
